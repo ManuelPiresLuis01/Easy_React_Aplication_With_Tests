e2a61ae35f47e3eab3667d4ded24fe1a1682dd51
manuelpiresluis <manuelpires@gmail.com>
2025-09-22 17:30:31 +0100

    refactor: improve task rendering tests for consistency and clarity

--- tests/e2e/type-input-test.spec.ts
+++ tests/e2e/type-input-test.spec.ts
@@ -4,54 +4,39 @@ test("should render 1 task", async ({ page }) => {
   await page.goto("/");
 
   const input = page.getByPlaceholder("Enter a task");
-
   await input.fill("New Task 1");
   await page.getByRole("button").click();
 
   const tasks = page.locator("div[class*='task']");
-  expect(tasks).toHaveCount(1);
+  await tasks.first().waitFor();
+  await expect(tasks).toHaveCount(1);
 });
 
-test("should render 2 task", async ({ page }) => {
+test("should render 2 tasks", async ({ page }) => {
   await page.goto("/");
 
   const input = page.getByPlaceholder("Enter a task");
-
   await input.fill("New Task 1");
   await page.getByRole("button").click();
   await input.fill("New Task 2");
   await page.getByRole("button").click();
 
   const tasks = page.locator("div[class*='task']");
-  expect(tasks).toHaveCount(2);
+  await tasks.first().waitFor();
+  await expect(tasks).toHaveCount(2);
 });
 
-test("should render 10 task", async ({ page }) => {
+test("should render 10 tasks", async ({ page }) => {
   await page.goto("/");
 
   const input = page.getByPlaceholder("Enter a task");
-
-  await input.fill("New Task 1");
-  await page.getByRole("button").click();
-  await input.fill("New Task 1");
-  await page.getByRole("button").click();
-  await input.fill("New Task 1");
-  await page.getByRole("button").click();
-  await input.fill("New Task 1");
-  await page.getByRole("button").click();
-  await input.fill("New Task 1");
-  await page.getByRole("button").click();
-  await input.fill("New Task 1");
-  await page.getByRole("button").click();
-  await input.fill("New Task 1");
-  await page.getByRole("button").click();
-  await input.fill("New Task 1");
-  await page.getByRole("button").click();
-  await input.fill("New Task 1");
-  await page.getByRole("button").click();
-  await input.fill("New Task 1");
-  await page.getByRole("button").click();
-
   const tasks = page.locator("div[class*='task']");
-  expect(tasks).toHaveCount(10);
+
+  for (let i = 1; i <= 10; i++) {
+    await input.fill(`New Task ${i}`);
+    await page.getByRole("button").click();
+  }
+
+  await tasks.first().waitFor();
+  await expect(tasks).toHaveCount(10);
 });
