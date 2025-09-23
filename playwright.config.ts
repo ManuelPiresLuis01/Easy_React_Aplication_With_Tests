d6a18c047d12f319a7ee1247b2e873c53167c093
manuelpiresluis <manuelpires@gmail.com>
2025-09-23 10:29:39 +0100

    feat: enable headless mode in Playwright configuration

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
   testDir: "./tests/e2e",
   timeout: 60 * 1000,
   use: {
-    headless: false,
+    headless: true,
     baseURL: "http://localhost:5173",
     viewport: { width: 1480, height: 520 },
   },
